fc995f5cc3a00c4e65162234cfb2ffd0e4126a00
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-04-08 23:39:30 -0400

    doc: add more details

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 #![no_std]
-///
-/// This crate provides a data structure to handle dynamic tree connectivity.
-/// Both incremental and decremental operations are supported with amortized O(log n)
-/// time complexity.
-///
-/// As the underlying data structure is a Splay tree, this crate works best with the situation
-/// where the working set is relatively small.
-///
+//!
+//! This crate provides a data structure to handle dynamic tree connectivity.
+//! Both incremental and decremental operations are supported with amortized O(log n)
+//! time complexity.
+//!
+//! As the underlying data structure is a Splay tree, this crate works best with the situation
+//! where the working set is relatively small.
+//!
+//! To represent a node in the forest, one can create a handle via [`Handle::new`].
+//! To connect two nodes, one can use [`Handle::connect`]. This will return a [`Connection`], which
+//! will keep the two nodes connected until it is dropped.
+//!
 extern crate alloc;
 
 use alloc::rc::Rc;
